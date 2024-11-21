21ae40ec41996baa18e6ed09ca7b8c94e6dd52de
amir-roshan <amirvalielahi@gmail.com>
2024-11-21 11:26:45 -0800

    implement movie fetching and genre retrieval in home component; remove unused logic from movie cards component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MovieCardsComponent } from '../movie-cards/movie-cards.component';
-import { RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment.development';
 
 @Component({
   selector: 'app-home',
@@ -9,4 +11,72 @@ import { RouterModule } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent implements OnInit {
+  private _http: HttpClient;
+  movies: any[] = [];
+  genres: any[] = [];
+
+  constructor(private http: HttpClient, private route: ActivatedRoute) {
+    this._http = http;
+  }
+
+  ngOnInit(): void {
+    this.fetchMovies();
+    this.fetchMovieGenres();
+    this.getDateRange();
+    this.route.params.subscribe((params) => {
+      console.log('Route parameters:', params);
+    });
+  }
+
+  getDateRange() {
+    let today = new Date();
+    this.getFormattedDate(today);
+    let sixtyDaysAgo = new Date();
+    sixtyDaysAgo.setDate(sixtyDaysAgo.getDate() - 60);
+    this.getFormattedDate(sixtyDaysAgo);
+  }
+
+  getFormattedDate(dt: Date) {
+    const formatTwoDigits = (num: number) => (num < 10 ? '0' + num : num);
+
+    const day = formatTwoDigits(dt.getDate());
+    const month = formatTwoDigits(dt.getMonth() + 1);
+    const year = dt.getFullYear();
+
+    alert(`Date: ${day}/${month}/${year}`);
+  }
+
+  async fetchMovies(): Promise<void> {
+    const apiUrl = environment.BASE_URL + environment.API_KEY;
+
+    this.http.get(apiUrl).subscribe({
+      next: (response: any) => {
+        this.movies = response.results;
+        console.log(this.movies);
+      },
+      error: (error) => {
+        console.error('Error fetching movies:', error);
+      },
+      complete: () => {
+        console.log('Movie fetch complete');
+      },
+    });
+  }
+
+  async fetchMovieGenres(): Promise<void> {
+    const apiUrl = environment.GENRE_URL + environment.API_KEY;
+    this.http.get(apiUrl).subscribe({
+      next: (response: any) => {
+        this.genres = response.genres;
+        console.log(this.genres);
+      },
+      error: (error) => {
+        console.error('Error fetching genres:', error);
+      },
+      complete: () => {
+        console.log('Genre fetch complete');
+      },
+    });
+  }
+}
--- src/app/movie-cards/movie-cards.component.ts
+++ src/app/movie-cards/movie-cards.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment.development';
+import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-movie-cards',
@@ -11,35 +8,4 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './movie-cards.component.html',
   styleUrl: './movie-cards.component.css',
 })
-export class MovieCardsComponent implements OnInit {
-  private _http: HttpClient;
-  movies: any[] = [];
-
-  constructor(private http: HttpClient, private route: ActivatedRoute) {
-    this._http = http;
-  }
-
-  ngOnInit(): void {
-    this.fetchMovies();
-    this.route.params.subscribe((params) => {
-      console.log('Route parameters:', params);
-    });
-  }
-
-  async fetchMovies(): Promise<void> {
-    const apiUrl = environment.BASE_URL + environment.API_KEY;
-
-    this.http.get(apiUrl).subscribe({
-      next: (response: any) => {
-        this.movies = response.results;
-        console.log(this.movies);
-      },
-      error: (error) => {
-        console.error('Error fetching movies:', error);
-      },
-      complete: () => {
-        console.log('Movie fetch complete');
-      },
-    });
-  }
-}
+export class MovieCardsComponent {}
