ba0ed70dda8761ea60f38ec2d53d4f3d98a7cb84
amir-roshan <amirvalielahi@gmail.com>
2024-11-21 16:25:47 -0800

    refactor navigation links; convert buttons to anchor tags for improved accessibility and styling

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,1 @@
-export const environment = {
-  production: true,
-  API_KEY: "65b6908b7d5f369cbb1d63f4cec2fc40",
-  BASE_URL:"http://api.themoviedb.org/3/discover/movie?api_key=",
-  GENRE_URL: "https://api.themoviedb.org/3/genre/movie/list?api_key=",
-  SELECT_GENRE: "&with_genres=",
-};
+export const environment = {};
